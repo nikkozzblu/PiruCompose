ef624467f6153dc35531a47bb1b00c0a18ec945b
PiruU <36201929+PiruU@users.noreply.github.com>
2019-10-15 21:25:02 +0200

    Update test_strings_instrument.py

--- modules/strings_instrument/tests/test_strings_instrument.py
+++ modules/strings_instrument/tests/test_strings_instrument.py
@@ -4,9 +4,16 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import strings_instrument
 
-
-def test_play_single_note_natural_c():
+def test_play_full_palm_mute_guitar():
     tested_instrument = strings_instrument.make_regular_guitar()
-    i_natural_c = 39
-    a_string, c_fret = 1, 3
-    return tested_instrument.play_single_note(a_string, c_fret) == i_natural_c
+    return tested_instrument.play_strings() == [None] * 6
+
+def test_play_natural_c_guitar():
+    tested_instrument = strings_instrument.make_regular_guitar()
+    tested_instrument.press_strings([None, 3, None, None, None, None])
+    return tested_instrument.play_strings() == [None, 39, None, None, None, None]
+
+def test_play_regular_c_chord_guitar():
+    tested_instrument = strings_instrument.make_regular_guitar()
+    tested_instrument.press_strings([None, 3, 2, 0, 1, None])
+    return tested_instrument.play_strings() == [None, 39, 43, 46, 51, None]
