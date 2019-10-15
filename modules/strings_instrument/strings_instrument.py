c48b2feadc04193a33a23e4e14745c1f26186221
PiruU <36201929+PiruU@users.noreply.github.com>
2019-10-15 21:23:54 +0200

    Update strings_instrument.py

--- modules/strings_instrument/strings_instrument.py
+++ modules/strings_instrument/strings_instrument.py
@@ -1,6 +1,5 @@
 import tuning
-import keyboard
-
+import note
 
 def make(tuning = tuning.regular_guitar):
     return Strings_instrument().set_tuning(tuning)
@@ -8,19 +7,35 @@ def make(tuning = tuning.regular_guitar):
 def make_regular_guitar():
     return make(tuning = tuning.regular_guitar)
 
-
 class Strings_instrument:
     """
     class String_instrument describes an instrument with strings and frets
     """
     def __init__(self):
-        self._tuning = tuning.regular_guitar
+        self._tuning = note.get_keyboard_indices(tuning.regular_guitar)
+        self.init_frets()
 
-    def set_tuning(self, tuning):
-        self._tuning = tuning
+    def count_strings(self):
+        return len(self._tuning)
+
+    def init_frets(self):
+        self._frets = [None] * self.count_strings()
         return self
 
-    def play_single_note(self, i_string, i_fret) :
-        open_string_tag = self._tuning[i_string]
-        return keyboard.notes[open_string_tag] + i_fret if i_fret != None else None
+    def set_tuning(self, tuning):
+        self._tuning = note.get_keyboard_indices(tuning)
+        self.init_frets()
+        return self
 
+    def press_strings(self, frets):
+        if len(frets) == self.count_strings():
+           self._frets = frets
+        return self
+
+    def play_string(self, i_string):
+        i_fret = self._frets[i_string]
+        return self._tuning[i_string] + i_fret if i_fret != None else None
+
+    def play_strings(self):
+        n_strings = self.count_strings()
+        return [self.play_string(i_string) for i_string in range(n_strings)]
